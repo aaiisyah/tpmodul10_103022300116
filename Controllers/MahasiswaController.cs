b396f6a27fef239c17df266393ec1341acbd98fb
aaiisyah <aaiisyah2007@gmail.com>
2025-04-27 18:57:11 +0700

    menambahkan API mahasiswa

---
+++ Controllers/MahasiswaController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using tpmodul10_103022300116.Models;
+
+namespace tpmodul10_103022300116.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaController : ControllerBase
+    {
+        private static List<Mahasiswa> mahasiswaList = new List<Mahasiswa>
+        {
+            new Mahasiswa {Nama = "Aisyah", Nim = "103022300116"},
+            new Mahasiswa {Nama = "Apriliani", Nim = "103022300052"},
+            new Mahasiswa {Nama = "Deswita Syaharani", Nim = "103022300004"},
+            new Mahasiswa {Nama = "Nadya Aulia Salma", Nim = "103022300022"}
+        };
+
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return mahasiswaList;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Mahasiswa> Get(int id)
+        {
+            if (id < 0 || id >= mahasiswaList.Count)
+            {
+                return NotFound();
+            }
+            return mahasiswaList[id];
+        }
+
+        [HttpPost]
+        public IActionResult Post(Mahasiswa mhs)
+        {
+            mahasiswaList.Add(mhs);
+            return CreatedAtAction(nameof(Get), new { id = mahasiswaList.Count - 1 }, mhs);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        { 
+            if (id < 0 || id >= mahasiswaList.Count)
+            {
+                return NotFound();
+            }
+            mahasiswaList.RemoveAt(id);
+            return NoContent();
+
+        }
+
+    }
+}
---
+++ Models/Mahasiswa.cs
@@ -0,0 +1,10 @@
+﻿namespace tpmodul10_103022300116.Models
+{
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string Nim { get; set; }
+    }
+}
+
+
